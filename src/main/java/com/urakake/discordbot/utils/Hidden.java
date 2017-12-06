7b55897b67b6d193b67a7fff2c3d6b93fae87673
ZachPingo <33404566+ZachPingo@users.noreply.github.com>
2017-12-06 17:52:05 -0600

    Update Hidden.java

--- src/main/java/com/urakake/discordbot/utils/Hidden.java
+++ src/main/java/com/urakake/discordbot/utils/Hidden.java
@@ -1,5 +1,5 @@
 package com.urakake.discordbot.utils;
 
 public class Hidden {
-    public static String botToken = "Mzc3Mzc2MzAwNjE4MjE5NTQy.DQoBxg.j_ijYg0pI7p7erjcG0YxA3JqmUw";
+    public static String botToken = "Mzc2ODg0NjQwNjIxMDAyNzYy.DQoRww.hvpSn0XKsabN7fRn3KfeaCWYL5k";
 }
